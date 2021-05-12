b768f5ed56b474e5d228bf811f9a760691d7ef2c
pj1130 <precious_lapuos@informatics.edu.ph>
2021-05-13 03:45:39 +0800

    Activity3

---
+++ ProgAct3/studentController.js
@@ -0,0 +1,85 @@
+mainApp.controller("studentController", function($scope) {
+    $scope.student1 = {
+       firstName: "Mahesh",
+       lastName: "Parashar",
+       strand: "STEM",
+       school:"INFO",
+       level:"GR 12",
+    
+       subject:[
+          {name:'Physics',marks:70},
+          {name:'Chemistry',marks:80},
+          {name:'Math',marks:75},
+          {name:'English',marks:90},
+          {name:'History',marks:85}
+       ],
+       fullName: function() {
+          var Student1;
+          Student1 = $scope.student1;
+          return Student1.firstName + " " + Student1.lastName;
+       },
+
+       EdInfo: function(){
+        var Student1;
+        Student1 = $scope.student1;
+        return Student1.strand + " " + Student1.school + " " + Student1.level;
+       }
+     
+    }
+
+    $scope.student2 = {
+      firstName: "Jack",
+      lastName: "Rogers",
+      strand: "ABM",
+      school:"San Beda",
+      level:"11",
+   
+      subject:[
+         {name:'Physics',marks:82},
+          {name:'Chemistry',marks:84},
+          {name:'Math',marks:86},
+          {name:'English',marks:88},
+          {name:'History',marks:90}
+      ],
+      fullName: function() {
+         var Student2;
+         Student2 = $scope.student2;
+         return Student2.firstName + " " + Student2.lastName;
+      },
+
+      EdInfo: function(){
+       var Student2;
+       Student2 = $scope.student2;
+       return Student2.strand + " " + Student2.school + " " + Student2.level;
+      }
+    
+   }
+
+   $scope.student3 = {
+      firstName: "Koko",
+      lastName: "Bop",
+      strand: "HUMMS",
+      school:"FEU",
+      level:"11",
+   
+      subject:[
+         {name:'Physics',marks:90},
+          {name:'Chemistry',marks:91},
+          {name:'Math',marks:93},
+          {name:'English',marks:94},
+          {name:'History',marks:95}
+      ],
+      fullName: function() {
+         var Student3;
+         Student3 = $scope.student3;
+         return Student3.firstName + " " + Student3.lastName;
+      },
+
+      EdInfo: function(){
+       var Student3;
+       Student3 = $scope.student3;
+       return Student3.strand + " " + Student3.school + " " + Student3.level;
+      }
+    
+   };
+ });
